80a033373ba56b27a1e42514017c6cc31302d241
ShubhKakkar <86162630+ShubhKakkar@users.noreply.github.com>
2023-08-25 13:10:41 +0530

    Base structure

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.Secret_Key);
 
 app.use(
   cors({
@@ -17,7 +18,10 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb" }));
 
 // routes
+app.get("/", (req, res) => {
+  res.status(201).json("Store server");
+});
 
-app.listen('8080', () => {
-    console.log('Server listening at port 8080');
-})
+app.listen("8080", () => {
+  console.log("Server listening at port 8080");
+});
